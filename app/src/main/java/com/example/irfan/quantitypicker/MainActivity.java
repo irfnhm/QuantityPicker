6f871baf9fa1b86fc0e01ce6ba0365f725e7be68
Irfan Hamid <irfnhm@gmail.com>
2018-02-22 18:51:59 +0530

    Added new app files

---
+++ app/src/main/java/com/example/irfan/quantitypicker/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.irfan.quantitypicker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.view.View;
+
+import org.w3c.dom.Text;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    int quantity = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void increement(View view){
+        quantity = quantity + 1;
+        display(quantity);
+    }
+
+    public void decreement(View view){
+        quantity = quantity - 1;
+        display(quantity);
+    }
+
+    public void submitOrder(View view){
+        //displayPrice(quantity * 5);
+        String priceMessage = "Total: $" + quantity * 5;
+        priceMessage = priceMessage + "\nThank you!";
+        displayMessage(priceMessage);
+    }
+
+    private void display(int number){
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int number){
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    private void displayMessage(String message){
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+    }
+}
